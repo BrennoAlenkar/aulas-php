e50619f1dae9f09b41be3e0c829b5992055b5fd6
Brenno Alencar <127119853+BrennoAlenkar@users.noreply.github.com>
2025-01-02 20:01:22 -0300

    phpinfo

--- ex01/index.php
+++ ex01/index.php
@@ -3,14 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Curso | PHP</title>
+    <title>Dados do servidor</title>
 </head>
 <body>
-    <h1>
-        <?php 
-        echo "Ola, Mundo! \u{1f30e}";
-        ?>
-    </h1>
-    <p>Vamos aprender php</p>
+    <h1>Dados do servidor</h1>
+    <?php
+    phpinfo();
+    ?>
 </body>
 </html>
